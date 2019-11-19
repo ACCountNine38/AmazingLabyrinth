960e4ea6989ab6bc794d1054e664ef60e1e97080
Jaspe <Jaspe@10.0.0.56>
2019-11-18 22:16:01 -0500

    hello

--- AmazingLabyrinth/src/main/Launcher.java
+++ AmazingLabyrinth/src/main/Launcher.java
@@ -1,12 +1,14 @@
 package main;
 
+import states.GameState;
 import states.MenuState;
 
 public class Launcher {
 	
 	public static void main(String[] args) {
 		
-		new MenuState();
+		//new MenuState();
+		new GameState();
 		
 	}
 
